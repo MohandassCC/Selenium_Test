d31b92ee5abd2c912c1af82a7e504fc45f5832f6
Mohandass <mohandass.m@customercentria.com>
2020-03-16 11:57:31 +0530

    test path

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -21,8 +21,8 @@ public class TestUtil extends TestBase {
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 
-	public static String TESTDATA_SHEET_PATH = "${JENKINS_HOME}/workspace"
-			+ "//src//main//java//com//crm//qa//testdata//FreeCrmTestData.xlsx";
+	public static String TESTDATA_SHEET_PATH = "/workspace"
+			+ "FreeCrmTestData.xlsx";
 
 	static Workbook book;
 	static Sheet sheet;
